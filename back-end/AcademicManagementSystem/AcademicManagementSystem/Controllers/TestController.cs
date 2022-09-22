7c1bb587bc68a184e252402cb435b110c3e6651a
Nguyen Manh Tung <nmtung.study@gmail.com>
2022-09-22 14:58:56 +0700

    Add schedule page of student

--- back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/TestController.cs
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/TestController.cs
@@ -1,6 +1,8 @@
+using AcademicManagementSystem.Models;
 using Azure.Communication.Email;
 using Azure.Communication.Email.Models;
 using ClosedXML.Excel;
+using Google.Apis.Auth;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AcademicManagementSystem.Controllers;
@@ -80,5 +82,27 @@ public class TestController : ControllerBase
         SendEmailResult emailResult = emailClient.Send(emailMessage,CancellationToken.None);
 
         return Ok(emailResult);
+    }
+    
+    [HttpPost]
+    [Route("api/test/login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        var listEmail = new List<string> { "nmtung.temp@gmail.com", "nmtungofficial@gmail.com" };
+        
+        // verify token from client google
+        var payload = GoogleJsonWebSignature.ValidateAsync(request.Token,
+            new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = new[] { "518989199582-9ul4cerv67mgmg777fpl0jl4lb4nsnji.apps.googleusercontent.com" }
+            }).Result.Email;
+        
+        // check email in list email
+        if (listEmail.Contains(payload))
+        {
+            return Ok("Login success");
+        }
+        
+        return BadRequest("Login failed");
     }
 }
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,15 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.min.css';
 import { NextUIProvider } from '@nextui-org/react';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import viVN from 'antd/lib/locale/vi_VN';
 import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NextUIProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ConfigProvider locale={viVN}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ConfigProvider>
     </NextUIProvider>
   </React.StrictMode>
 );
