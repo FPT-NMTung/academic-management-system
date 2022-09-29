ff58043ab7147dfaca3817e3dfdeaf49d3b9129f
nguyenht <mrlimfo@gmail.com>
2022-09-29 08:47:43 +0700

    fix url path and bad request conditions

--- back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/AddressController.cs
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/AddressController.cs
@@ -26,7 +26,7 @@ public class AddressController : ControllerBase
         var listProvince = _context.Provinces.ToList()
             .Select(p => new ProvinceResponse() { Id = p.Id, Code = p.Code, Name = p.Name });
         // empty list
-        if (listProvince.Any())
+        if (!listProvince.Any())
         {
             return BadRequest(CustomResponse.BadRequest("Province not found", "address-error-000001"));
         }
@@ -35,43 +35,42 @@ public class AddressController : ControllerBase
     
     // get list district by province id
     [HttpGet]
-    [Route("api/address/district/{provinceId}")]
-    public IActionResult GetDistrict(int provinceId)
+    [Route("api/address/district/{province_id}")]
+    public IActionResult GetDistrict(int province_id)
     {
         var listDistrict = _context.Districts.ToList()
-            .Where(d => d.ProvinceId == provinceId)
+            .Where(d => d.ProvinceId == province_id)
             .Select(d =>
                 new DistrictResponse()
                 {
                     DistrictId = d.Id, Name = d.Name, Prefix = d.Prefix
                 });
-        
         // empty list
         if (!listDistrict.Any())
         {
             return BadRequest(CustomResponse
-                .BadRequest("District with this ProvinceId not found", "address-error-000002"));
+                .BadRequest("District with this province_id not found", "address-error-000002"));
         }
         return Ok(CustomResponse.Ok("Get list district success", listDistrict));
     }
     
     // get list ward by district id
     [HttpGet]
-    [Route("api/address/ward/{districtId}")]
-    public IActionResult GetWard(int districtId)
+    [Route("api/address/ward/{district_id}")]
+    public IActionResult GetWard(int district_id)
     {
         var listWard = _context.Wards.ToList()
-            .Where(w => w.DistrictId == districtId)
+            .Where(w => w.DistrictId == district_id)
             .Select(w =>
                 new WardResponse()
                 {
                     WardId = w.Id, Name = w.Name, Prefix = w.Prefix
                 });
         // empty list
-        if (listWard.Any())
+        if (!listWard.Any())
         { 
             return BadRequest(CustomResponse
-                .BadRequest("Ward with this DistrictId not found", "address-error-000003"));
+                .BadRequest("Ward with this district_id not found", "address-error-000003"));
         }
         
         return Ok(CustomResponse.Ok("Get list ward success", listWard));
