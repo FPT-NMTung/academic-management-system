d25620f3f5e68cc32daf4e14d92d37e470bd4b3a
nguyenht <mrlimfo@gmail.com>
2022-09-29 07:03:42 +0700

    add select list province

--- back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/AddressController.cs
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/AddressController.cs
@@ -1,4 +1,6 @@
 using AcademicManagementSystem.Context;
+using AcademicManagementSystem.Context.AmsModels;
+using AcademicManagementSystem.Models.AddressController;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AcademicManagementSystem.Controllers;
@@ -17,7 +19,15 @@ public class AddressController : ControllerBase
     [Route("api/address/province")]
     public IActionResult GetProvince()
     {
-        var listProvince = _context.Districts.ToList();
+        //var listProvince = from _context.Districts.ToList();
+        var listProvince = from p in _context.Provinces.ToList()
+            select new ProvinceResponse()
+            {
+                Id = p.Id,
+                Code = p.Code,
+                Name = p.Name
+            };
+
         return Ok(CustomResponse.Ok("Get list province success", listProvince));
     }
     
