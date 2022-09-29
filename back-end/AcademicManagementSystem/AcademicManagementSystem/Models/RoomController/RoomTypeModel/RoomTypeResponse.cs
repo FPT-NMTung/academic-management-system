35167c3c6a6790a632447768142eb85999990db4
thanhnm <nmthanh1306@gmail.com>
2022-09-29 15:45:35 +0700

    Add Room APIs

---
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Models/RoomController/RoomTypeModel/RoomTypeResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AcademicManagementSystem.Models.RoomController.RoomTypeModel;
+
+public class RoomTypeResponse
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    
+    [JsonPropertyName("value")]
+    public string Value { get; set; }
+}
