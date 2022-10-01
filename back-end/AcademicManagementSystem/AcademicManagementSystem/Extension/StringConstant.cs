bdc3f46b35e9fb4a4db9aa7192f8cb0f40d3a77d
nguyenht <mrlimfo@gmail.com>
2022-10-02 04:59:53 +0700

    add center
    do not check all input case yet

---
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Extension/StringConstant.cs
@@ -0,0 +1,6 @@
+namespace AcademicManagementSystem.Extension;
+
+public struct StringConstant
+{
+    
+}
---
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Models/CenterController/CreateCenterRequest.cs
@@ -0,0 +1,6 @@
+namespace AcademicManagementSystem.Models.CenterController;
+
+public class CreateCenterRequest
+{
+    
+}
