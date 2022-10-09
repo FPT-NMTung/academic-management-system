8414cb31a2de7204eb10760831f9e4a7b093a49f
thanhnm <nmthanh1306@gmail.com>
2022-10-09 20:54:16 +0700

    get class learning days and class status

---
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/ClassStatusController.cs
@@ -0,0 +1,31 @@
+﻿using AcademicManagementSystem.Context;
+using AcademicManagementSystem.Models.ClassStatusController;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademicManagementSystem.Controllers;
+
+[ApiController]
+public class ClassStatusController : ControllerBase
+{
+    private readonly AmsContext _context;
+
+    public ClassStatusController(AmsContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    [Route("api/class-statuses")]
+    [Authorize(Roles = "admin, sro")]
+    public IActionResult GetClassStatuses()
+    {
+        var classStatuses =
+            _context.ClassStatuses.Select(cs => new ClassStatusResponse()
+            {
+                Id = cs.Id,
+                Value = cs.Value
+            });
+        return Ok(CustomResponse.Ok("get all Class statuses successfully", classStatuses));
+    }
+}
