51b5d1318f951e119f49e2b66802b8d5b410f0ac
thanhnm <nmthanh1306@gmail.com>
2022-10-11 17:09:25 +0700

    Add Grade Category Module APIs

---
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Models/GradeItemController/GradeItemResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using AcademicManagementSystem.Models.GradeCategoryModule;
+
+namespace AcademicManagementSystem.Models.GradeItemController;
+
+public class GradeItemResponse
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+}
