e86bad57b8c4d41fb186e4edd78162d82df17224
Nguyen Manh Tung <nmtung.study@gmail.com>
2022-10-22 15:05:55 +0700

    Update module

--- front-end/src/screens/Sro/Student/StudentScreen.jsx
+++ front-end/src/screens/Sro/Student/StudentScreen.jsx
@@ -1,13 +1,27 @@
-import { Grid } from '@nextui-org/react';
+import { Card, Grid, Text } from '@nextui-org/react';
 
 const StudentScreen = () => {
   return (
-    <Grid.Container>
-      <Grid xs={2}>
-        <div>1</div>
+    <Grid.Container gap={2}>
+      <Grid xs={12}>
+        <Card variant="bordered">
+          <Card.Body
+            css={{
+              padding: 10,
+            }}
+          >
+            
+          </Card.Body>
+        </Card>
       </Grid>
-      <Grid xs={10}>
-        <div>2</div>
+      <Grid xs={12}>
+        <Card variant="bordered">
+          <Card.Header>
+            <Text b p size={14} css={{ width: '100%', textAlign: 'center' }}>
+              Danh sách học viên
+            </Text>
+          </Card.Header>
+        </Card>
       </Grid>
     </Grid.Container>
   );
