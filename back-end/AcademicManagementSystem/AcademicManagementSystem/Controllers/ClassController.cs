bc35480244e9b84e78e88889988411e1cc84b24c
nguyenht <mrlimfo@gmail.com>
2022-10-25 03:29:50 +0700

    AMS-5 add get all student in class and some check

--- back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/ClassController.cs
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/ClassController.cs
@@ -10,7 +10,11 @@ using AcademicManagementSystem.Models.CenterController;
 using AcademicManagementSystem.Models.ClassController;
 using AcademicManagementSystem.Models.ClassDaysController;
 using AcademicManagementSystem.Models.ClassStatusController;
+using AcademicManagementSystem.Models.CourseController;
 using AcademicManagementSystem.Models.CourseFamilyController;
+using AcademicManagementSystem.Models.GenderController;
+using AcademicManagementSystem.Models.RoleController;
+using AcademicManagementSystem.Models.UserController.StudentController;
 using AcademicManagementSystem.Services;
 using ClosedXML.Excel;
 using Microsoft.AspNetCore.Authorization;
@@ -574,6 +578,13 @@ public class ClassController : ControllerBase
     [Route("api/classes/{id:int}/students-from-excel")]
     public IActionResult SaveImportedStudents(int id)
     {
+        var existedClass = _context.Classes.Any(c => c.Id == id);
+        if (!existedClass)
+        {
+            var error = ErrorDescription.Error["E1073"];
+            return BadRequest(CustomResponse.BadRequest(error.Message, error.Type));
+        }
+
         var students = _context.Students
             .Include(s => s.StudentsClasses)
             .Where(s => s.StudentsClasses.Any(sc => sc.ClassId == id))
@@ -602,6 +613,13 @@ public class ClassController : ControllerBase
     [Route("api/classes/{id:int}/students-from-excel")]
     public IActionResult DeleteImportedStudents(int id)
     {
+        var existedClass = _context.Classes.Any(c => c.Id == id);
+        if (!existedClass)
+        {
+            var error = ErrorDescription.Error["E1073"];
+            return BadRequest(CustomResponse.BadRequest(error.Message, error.Type));
+        }
+
         var students = _context.Students
             .Include(s => s.StudentsClasses)
             .Where(s => s.StudentsClasses.Any(sc => sc.ClassId == id))
@@ -623,7 +641,97 @@ public class ClassController : ControllerBase
 
         return Ok(CustomResponse.Ok("Cancel import students successfully", null!));
     }
-    
+
+    // get students in class
+    [HttpGet]
+    [Route("api/classes/{id:int}/students")]
+    public IActionResult GetStudentsInClass(int id)
+    {
+        // is class exist
+        var existedClass = _context.Classes.Any(c => c.Id == id);
+        if (!existedClass)
+        {
+            var error = ErrorDescription.Error["E1073"];
+            return BadRequest(CustomResponse.BadRequest(error.Message, error.Type));
+        }
+
+        var students = GetAllStudentsByClassId(id);
+        return Ok(CustomResponse.Ok("Get students in class successfully", students));
+    }
+
+    private List<StudentResponse> GetAllStudentsByClassId(int id)
+    {
+        var students = _context.Users.Include(u => u.Student)
+            .Include(u => u.Student.Course)
+            .Include(u => u.Student.Course.CourseFamily)
+            .Include(u => u.Province)
+            .Include(u => u.District)
+            .Include(u => u.Ward)
+            .Include(u => u.Center)
+            .Include(u => u.Role)
+            .Include(u => u.Gender)
+            .Where(u => u.RoleId == RoleIdStudent && u.Student.StudentsClasses.Any(sc => sc.ClassId == id))
+            .Select(u => new StudentResponse()
+            {
+                UserId = u.Student.UserId, Promotion = u.Student.Promotion, Status = u.Student.Status,
+                University = u.Student.University, ApplicationDate = u.Student.ApplicationDate,
+                ApplicationDocument = u.Student.ApplicationDocument, CompanyAddress = u.Student.CompanyAddress,
+                CompanyPosition = u.Student.CompanyPosition, CompanySalary = u.Student.CompanySalary,
+                ContactAddress = u.Student.ContactAddress, ContactPhone = u.Student.ContactPhone,
+                CourseCode = u.Student.CourseCode, EnrollNumber = u.Student.EnrollNumber,
+                FacebookUrl = u.Student.FacebookUrl, FeePlan = u.Student.FeePlan, HighSchool = u.Student.HighSchool,
+                HomePhone = u.Student.HomePhone, ParentalName = u.Student.ParentalName,
+                ParentalRelationship = u.Student.ParentalRelationship, ParentalPhone = u.Student.ParentalPhone,
+                PortfolioUrl = u.Student.PortfolioUrl, StatusDate = u.Student.StatusDate,
+                WorkingCompany = u.Student.WorkingCompany, IsDraft = u.Student.IsDraft, Avatar = u.Avatar,
+
+                FirstName = u.FirstName, LastName = u.LastName, Birthday = u.Birthday, Email = u.Email,
+                MobilePhone = u.MobilePhone, CenterId = u.CenterId, EmailOrganization = u.EmailOrganization,
+                CenterName = u.Center.Name, CreatedAt = u.CreatedAt, UpdatedAt = u.UpdatedAt,
+                CitizenIdentityCardNo = u.CitizenIdentityCardNo,
+                CitizenIdentityCardPublishedDate = u.CitizenIdentityCardPublishedDate,
+                CitizenIdentityCardPublishedPlace = u.CitizenIdentityCardPublishedPlace,
+
+                Course = new CourseResponse()
+                {
+                    Code = u.Student.Course.Code, Name = u.Student.Course.Name,
+                    SemesterCount = u.Student.Course.SemesterCount,
+                    CourseFamilyCode = u.Student.Course.CourseFamilyCode, IsActive = u.Student.Course.IsActive,
+                    CreatedAt = u.Student.Course.CreatedAt,
+                    UpdatedAt = u.Student.Course.UpdatedAt, CourseFamily = new CourseFamilyResponse()
+                    {
+                        Code = u.Student.Course.CourseFamily.Code, Name = u.Student.Course.CourseFamily.Name,
+                        IsActive = u.Student.Course.CourseFamily.IsActive,
+                        PublishedYear = u.Student.Course.CourseFamily.PublishedYear,
+                        CreatedAt = u.Student.Course.CourseFamily.CreatedAt,
+                        UpdatedAt = u.Student.Course.CourseFamily.UpdatedAt
+                    }
+                },
+                Province = new ProvinceResponse()
+                {
+                    Id = u.Province.Id, Name = u.Province.Name, Code = u.Province.Code
+                },
+                District = new DistrictResponse()
+                {
+                    Id = u.District.Id, Name = u.District.Name, Prefix = u.District.Prefix
+                },
+                Ward = new WardResponse()
+                {
+                    Id = u.Ward.Id, Name = u.Ward.Name, Prefix = u.Ward.Prefix
+                },
+                Gender = new GenderResponse()
+                {
+                    Id = u.Gender.Id, Value = u.Gender.Value
+                },
+                Role = new RoleResponse()
+                {
+                    Id = u.Role.Id, Value = u.Role.Value
+                }
+            }).ToList();
+        return students;
+    }
+
+    // download template
     [HttpGet]
     [Route("api/class/download-template-import-students")]
     public IActionResult DownloadTemplateStudents()
@@ -636,7 +744,8 @@ public class ClassController : ControllerBase
             {
                 workbook.SaveAs(stream);
                 var content = stream.ToArray();
-                return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Template_Student.xlsx");
+                return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    "Template_Student.xlsx");
             }
         }
     }
