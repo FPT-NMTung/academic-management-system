7fa29a4cf5884dfafca455fcf28ef4a013285ce8
nguyenht <mrlimfo@gmail.com>
2022-11-02 08:18:41 +0700

    Update migration - add isActive to StudentClass

---
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Migrations/20221102010819_Add isActive to StudentClass.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademicManagementSystem.Migrations
+{
+    public partial class AddisActivetoStudentClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "student_class",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "student_class");
+        }
+    }
+}
