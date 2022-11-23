2f5db051323bd8276288c06a29dc39ad8b8d02b8
thanhnm <nmthanh1306@gmail.com>
2022-11-23 14:26:45 +0700

    AMS-31 view attendances for all sessions or specific session on schedule

---
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/AttendanceStatusController.cs
@@ -0,0 +1,30 @@
+﻿using AcademicManagementSystem.Context;
+using AcademicManagementSystem.Models.AttendanceStatusController.AttendanceStatusModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademicManagementSystem.Controllers;
+
+[ApiController]
+public class AttendanceStatusController : ControllerBase
+{
+    private readonly AmsContext _context;
+
+    public AttendanceStatusController(AmsContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    [Route("api/attendance-statuses")]
+    [Authorize (Roles = "sro, teacher")]
+    public IActionResult GetAttendanceStatuses()
+    {
+        var statuses = _context.AttendanceStatuses.Select(s => new AttendanceStatusResponse()
+        {
+            Id = s.Id,
+            Value = s.Value
+        }).ToList();
+        return Ok(CustomResponse.Ok("Get Attendance Statuses successfully", statuses));
+    }
+}
---
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Models/BasicResponse/BasicStudentResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AcademicManagementSystem.Models.BasicResponse;
+
+public class BasicStudentResponse
+{
+    [JsonPropertyName("user_id")]
+    public int UserId { get; set; }
+    
+    [JsonPropertyName("enroll_number")]
+    public string? EnrollNumber { get; set; }
+    
+    [JsonPropertyName("email_organization")]
+    public string? EmailOrganization { get; set; }
+    
+    [JsonPropertyName("first_name")]
+    public string FirstName { get; set; }
+    
+    [JsonPropertyName("last_name")]
+    public string LastName { get; set; }
+}
