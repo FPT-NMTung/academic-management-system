0400619b51188fae90010e9cb43a8e920d79bcd9
thanhnm <nmthanh1306@gmail.com>
2022-12-12 13:38:47 +0700

    AMS-35 Api get pass rate of teacher in all module that teach by teacher

--- back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/StudentGradeController.cs
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/StudentGradeController.cs
@@ -950,13 +950,15 @@ public class StudentGradeController : ControllerBase
         var modules = _context.ClassSchedules
             .Include(cs => cs.Module)
             .Where(cs => cs.TeacherId == teacherId)
-            .Select(cs => new BasicModuleResponse()
-            {
-                Id = cs.Module.Id,
-                Name = cs.Module.ModuleName,
-            });
+            .Select(cs => cs.Module).Distinct();
 
-        return Ok(CustomResponse.Ok("Get modules teach by teacher successfully", modules));
+        var response = modules.Select(m => new BasicModuleResponse()
+        {
+            Id = m.Id,
+            Name = m.ModuleName
+        });
+
+        return Ok(CustomResponse.Ok("Get modules teach by teacher successfully", response));
     }
 
     [HttpGet]
@@ -974,6 +976,67 @@ public class StudentGradeController : ControllerBase
         var classes = GetClassesTeachByTeacherByModuleId(teacherId, moduleId);
 
         return Ok(CustomResponse.Ok("Get classes of teacher by module successfully", classes));
+    }
+
+    [HttpGet]
+    [Route("api/statistics/teachers/{teacherId:int}/pass-rate-all-module")]
+    [Authorize(Roles = "sro")]
+    public IActionResult GetPassRateAllModuleOfTeacher(int teacherId)
+    {
+        // modules that teach by this teacher
+        var modules = _context.ClassSchedules
+            .Include(cs => cs.Module)
+            .Where(cs => cs.TeacherId == teacherId)
+            .Select(cs => cs.Module).Distinct().ToList();
+
+        int numberOfStudentInAllModule = 0;
+        int passedCount = 0;
+        foreach (var module in modules)
+        {
+            var classes = GetClassesTeachByTeacherByModuleId(teacherId, module.Id).ToList();
+            foreach (var c in classes)
+            {
+                var clazz = new Class()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                };
+
+                var progressScores = GetGradesOfStudentsInClass(clazz)
+                    .FirstOrDefault(m => m.Module.Id == module.Id);
+
+                if (progressScores == null)
+                {
+                    continue;
+                }
+
+                var students = progressScores.Students;
+                numberOfStudentInAllModule += students.Count;
+
+                foreach (var student in students)
+                {
+                    var totalScore = CalculateAverageScore(student, module);
+                    Console.WriteLine("student: " + student.UserId + " total score: " + totalScore);
+                    if (totalScore != null)
+                    {
+                        var roundedScore = Math.Round((double)totalScore, 2); // round to 2 decimal places
+
+                        if (roundedScore >= GradeToPass)
+                        {
+                            passedCount++;
+                        }
+                    }
+                }
+            }
+        }
+
+        var response = new PassRateOfTeacherInAllModuleResponse()
+        {
+            NumberOfAllStudents = numberOfStudentInAllModule,
+            NumberOfPassStudents = passedCount
+        };
+
+        return Ok(CustomResponse.Ok("Get pass rate all module of teacher successfully", response));
     }
 
     [HttpGet]
@@ -1322,67 +1385,6 @@ public class StudentGradeController : ControllerBase
             });
     }
 
-    // get grades of all students in class
-    // private IQueryable<ListStudentGradeResponse> GetGradesOfStudentsInClass(Class clazz, int moduleId)
-    // {
-    //     return _context.Modules
-    //         .Include(m => m.ClassSchedules)
-    //         .ThenInclude(cs => cs.Class)
-    //         .ThenInclude(c => c.StudentsClasses)
-    //         .Include(m => m.GradeCategoryModule)
-    //         .ThenInclude(gcm => gcm.GradeCategory)
-    //         .Include(m => m.GradeCategoryModule)
-    //         .ThenInclude(gcm => gcm.GradeItems)
-    //         .ThenInclude(gi => gi.StudentGrades)
-    //         .Where(m => m.ClassSchedules.Any(cs => cs.ClassId == clazz.Id && cs.ModuleId == m.Id))
-    //         .Select(m => new ListStudentGradeResponse()
-    //         {
-    //             Class = new BasicClassResponse()
-    //             {
-    //                 Id = clazz.Id,
-    //                 Name = clazz.Name
-    //             },
-    //
-    //             Module = new BasicModuleResponse()
-    //             {
-    //                 Id = m.Id,
-    //                 Name = m.ModuleName
-    //             },
-    //
-    //             Students = m.ClassSchedules.SelectMany(cs => cs.Class.StudentsClasses)
-    //                 .Where(cs => cs.ClassId == clazz.Id && cs.IsActive && !cs.Student.IsDraft)
-    //                 .Select(sc => new StudentInfoAndGradeResponse()
-    //                 {
-    //                     UserId = sc.Student.UserId,
-    //                     EnrollNumber = sc.Student.EnrollNumber,
-    //                     EmailOrganization = sc.Student.User.EmailOrganization,
-    //                     FirstName = sc.Student.User.FirstName,
-    //                     LastName = sc.Student.User.LastName,
-    //                     Avatar = sc.Student.User.Avatar,
-    //                     GradeCategories = m.GradeCategoryModule
-    //                         .Select(gcm => new GradeCategoryWithItemsResponse()
-    //                         {
-    //                             Id = gcm.GradeCategory.Id,
-    //                             Name = gcm.GradeCategory.Name,
-    //                             TotalWeight = gcm.TotalWeight,
-    //                             QuantityGradeItem = gcm.QuantityGradeItem,
-    //                             GradeItems = gcm.GradeItems
-    //                                 .Select(gi => new GradeItemWithStudentScoreResponse()
-    //                                 {
-    //                                     Id = gi.Id,
-    //                                     Name = gi.Name,
-    //                                     Grade = gi.StudentGrades.FirstOrDefault(sg =>
-    //                                         sg.StudentId == sc.Student.UserId && sg.ClassId == clazz.Id)!.Grade,
-    //                                     Comment = gi.StudentGrades.FirstOrDefault(sg =>
-    //                                         sg.StudentId == sc.Student.UserId && sg.ClassId == clazz.Id)!.Comment
-    //                                 })
-    //                                 .ToList()
-    //                         }).ToList()
-    //                 }).ToList()
-    //         });
-    // }
-
-
     private IQueryable<ListStudentGradeResponse> GetGradesOfStudentsInClass(Class clazz)
     {
         return _context.Modules
@@ -1502,59 +1504,4 @@ public class StudentGradeController : ControllerBase
                     }).ToList()
             });
     }
-
-    // private IQueryable<StudentGradeResponse> GetGradesOfSpecificStudent(Class clazz, int moduleId, Student student)
-    // {
-    //     return _context.Modules
-    //         .Include(m => m.GradeCategoryModule)
-    //         .ThenInclude(gcm => gcm.GradeCategory)
-    //         .Include(m => m.GradeCategoryModule)
-    //         .ThenInclude(gcm => gcm.GradeItems)
-    //         .ThenInclude(gi => gi.StudentGrades)
-    //         .ThenInclude(sg => sg.Class)
-    //         .Where(m => m.Id == moduleId)
-    //         .Select(m => new StudentGradeResponse()
-    //         {
-    //             Class = new BasicClassResponse()
-    //             {
-    //                 Id = clazz.Id,
-    //                 Name = clazz.Name
-    //             },
-    //
-    //             Module = new BasicModuleResponse()
-    //             {
-    //                 Id = m.Id,
-    //                 Name = m.ModuleName
-    //             },
-    //
-    //             Student = new StudentInfoAndGradeResponse()
-    //             {
-    //                 UserId = student.UserId,
-    //                 EnrollNumber = student.EnrollNumber,
-    //                 EmailOrganization = student.User.EmailOrganization,
-    //                 FirstName = student.User.FirstName,
-    //                 LastName = student.User.LastName,
-    //                 Avatar = student.User.Avatar,
-    //                 GradeCategories = m.GradeCategoryModule
-    //                     .Select(gcm => new GradeCategoryWithItemsResponse()
-    //                     {
-    //                         Id = gcm.GradeCategory.Id,
-    //                         Name = gcm.GradeCategory.Name,
-    //                         TotalWeight = gcm.TotalWeight,
-    //                         QuantityGradeItem = gcm.QuantityGradeItem,
-    //                         GradeItems = gcm.GradeItems
-    //                             .Select(gi => new GradeItemWithStudentScoreResponse()
-    //                             {
-    //                                 Id = gi.Id,
-    //                                 Name = gi.Name,
-    //                                 Grade = gi.StudentGrades.FirstOrDefault(sg =>
-    //                                     sg.StudentId == student.UserId && sg.ClassId == clazz.Id)!.Grade,
-    //                                 Comment = gi.StudentGrades.FirstOrDefault(sg =>
-    //                                     sg.StudentId == student.UserId && sg.ClassId == clazz.Id)!.Comment
-    //                             })
-    //                             .ToList()
-    //                     }).ToList()
-    //             },
-    //         });
-    // }
 }
