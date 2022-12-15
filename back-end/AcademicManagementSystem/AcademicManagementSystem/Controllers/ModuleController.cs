a9c3413d74566e7774fcd7390b5fd4485d30c98c
thanhnm <nmthanh1306@gmail.com>
2022-12-15 15:47:37 +0700

    Update grade category only when change module exam type

--- back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/ModuleController.cs
+++ back-end/AcademicManagementSystem/AcademicManagementSystem/Controllers/ModuleController.cs
@@ -403,6 +403,8 @@ public class ModuleController : ControllerBase
             return NotFound(CustomResponse.NotFound("Module not found"));
         }
 
+        var isChangeExamType = request.ExamType != module.ExamType;
+
         var canUpdate = CanDeleteUpdate(id);
         if (!canUpdate)
         {
@@ -466,7 +468,10 @@ public class ModuleController : ControllerBase
             return BadRequest(CustomResponse.BadRequest(error.Message, error.Type));
         }
 
-        AddDataToGradeCategoryModule(module.Id, request.ExamType);
+        if (isChangeExamType)
+        {
+            AddDataToGradeCategoryModule(module.Id, request.ExamType);
+        }
 
         return Ok(CustomResponse.Ok("Module updated successfully", updatedModule));
     }
